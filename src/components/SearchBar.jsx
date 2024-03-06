fc12ccbfe494e02d3da2461ce05e4bfe9fe2c4e6
WAQAS <muhammadwaqas0349@gmail.com>
2024-03-06 18:23:00 +0100

    first

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import axios from "axios";
+
+import "./SearchBar.css";
+
+export const SearchBar = ({ setResults }) => {
+  
+  const [input, setInput] = useState("");
+
+  const fetchData = (value) => {
+    axios.get("https://jsonplaceholder.typicode.com/users")
+    .then( (res) => {
+      const results = res.data.filter( (user) => {
+        return(
+          value && user && user.name && 
+          user.name.toLowerCase().includes(value));
+    })
+    setResults(results); 
+  })
+  }
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  };
+
+  return (
+    <div className="input-wrapper">
+      <FaSearch id="search-icon" />
+      <input
+        placeholder="Type to search..."
+        value={input}
+        onChange={(e) => handleChange(e.target.value)}
+      />
+    </div>
+  );
+};
